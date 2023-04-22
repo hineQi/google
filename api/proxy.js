a0ddd9ca90e132ff93b9c42f3d5d3b1aed3667d4
Hine <784311112@qq.com>
2023-04-23 04:10:58 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.youtube.com/";
+  let target = "https://api.openai.com/";
  
   // 代理目标地址
   // 将请求路径中的第一个参数作为目标地址的主机名
@@ -24,7 +24,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/backend`
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-//         "^/google.com/": "/",
+
     },
   })(req, res);
 };
